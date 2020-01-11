2cb655e8929c9209533f7c612be0da8d230a5ac6
gwillz <me@gwilyn.com>
2020-01-12 01:23:37 +1030

    Added parser tests.

--- src/parsers.rs
+++ src/parsers.rs
@@ -25,7 +25,7 @@ lazy_static! {
     
     // Capture everything after the season/episode identifier.
     static ref EPISODE_NAME: Regex =
-        Regex::new(r"(?i:s\d+e\d+|\d+of\d+|\d+x\d+)(.+)\.(?i:[a-z0-9]+)$")
+        Regex::new(r"(?i:s\d+e\d+|\d+of\d+|\d+x\d+|(?i:ep|episode|part)\W*\d+)(.+)\.(?i:[a-z0-9]+)$")
         .unwrap();
     
     // Capture the episode number by --of--
@@ -142,4 +142,140 @@ pub fn parse_extension(path: &str) -> Option<String> {
     EXTENSION
         .captures(path)
         .map(|m| String::from(&m[1]))
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn test_show_name_1() {
+        let actual = parse_show_name("Friends.5x02.whatever.mp4");
+        let expected = "Friends.";
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_show_name_2() {
+        let actual = parse_show_name("Friends.series.5.2of9.mp4");
+        let expected = "Friends.";
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    // This returns 'series 5' which is kind of correct. But I can't think
+    // of a better regex right now.
+    // #[test]
+    // fn test_show_name_bad() {
+    //     let actual = parse_show_name("series.5.2of9.mp4");
+    //     assert_eq!(actual, None);
+    // }
+    
+    #[test]
+    fn test_episode_name_1() {
+        let actual = parse_episode_name("Friends.2of9.one.with.ross.mp4");
+        let expected = ".one.with.ross";
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_episode_name_2() {
+        let actual = parse_episode_name("Friends.5x2.one.with.ross.mp4");
+        let expected = ".one.with.ross";
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_episode_name_3() {
+        let actual = parse_episode_name("Friends.s05e02.one.with.ross.mp4");
+        let expected = ".one.with.ross";
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_episode_name_4() {
+        let actual = parse_episode_name("Friends.part.2.one.with.ross.mp4");
+        let expected = ".one.with.ross";
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_episode_name_bad() {
+        let actual = parse_episode_name("friends.5x2.mp4");
+        assert_eq!("", actual.unwrap());
+    }
+    
+    #[test]
+    fn test_extension() {
+        let actual = parse_extension("Friends.5x2.whatever-lol.mp4");
+        let expected = "mp4";
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_extension_bad() {
+        let actual = parse_extension("well well well-5x2-avi");
+        assert_eq!(actual, None);
+    }
+    
+    #[test]
+    fn test_season_number_by_name() {
+        let actual = parse_season_number("Friends.series.5.ep.2.mp4");
+        let expected = 5;
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_season_number_by_se() {
+        let actual = parse_season_number("Friends.s05e02.mp4");
+        let expected = 5;
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_season_number_by_x() {
+        let actual = parse_season_number("Friends.5x2.mp4");
+        let expected = 5;
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_season_number_bad() {
+        let actual = parse_season_number("friends - yep.mp4");
+        assert_eq!(actual, None);
+    }
+    
+    #[test]
+    fn test_episode_number_by_of() {
+        let actual = parse_episode_number("Grand.Designs.Series.5.2of9.mp4");
+        let expected = 2;
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_episode_number_by_se() {
+        let actual = parse_episode_number("Grand.Designs.s05e02.mp4");
+        let expected = 2;
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_episode_number_by_x() {
+        let actual = parse_episode_number("Grand.Designs.5x2.mp4");
+        let expected = 2;
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_episode_number_by_name() {
+        let actual = parse_episode_number("Grand.Designs.ep2.mp4");
+        let expected = 2;
+        assert_eq!(expected, actual.unwrap());
+    }
+    
+    #[test]
+    fn test_episode_number_bad() {
+        let actual = parse_episode_number("friends - yep.mp4");
+        assert_eq!(actual, None);
+    }
 }
